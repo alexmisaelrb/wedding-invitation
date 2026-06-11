41a5726e91f8df84618eb5300a59e2cb78183af8
Alexis Ronderos <amrb.alex@outlook.com>
2026-06-10 22:13:05 -0600

    dinamyc port

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use("/api/invitados", invitadosRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor iniciado en puerto $(PORT)`);
 });
